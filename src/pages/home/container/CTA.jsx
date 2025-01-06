c01ac18b2371b573e0ab2a35bc80af7560a14c25
ABDULLAHAHMED1575 <abdullahmed1575@gmail.com>
2025-01-06 18:11:36 +0500

    CTA Completed

--- src/pages/home/container/CTA.jsx
+++ src/pages/home/container/CTA.jsx
@@ -6,31 +6,37 @@ export const CTA = () => {
     <>
         <div className='w-full h-auto max-h-24 md:max-h-40 aspect-3/2 bg-dark-hard mask-curved' style={{ '--c': '70px' }}></div>
         <section className='relative bg-dark-hard px-5'>
-            <div className='container grid grid-cols-12 mx-auto py-10'>
-                <div className='col-span-12'>
-                    <h2 className='text-white font-roboto font-bold text-2xl'>
+            <div className='container grid grid-cols-12 mx-auto py-10 md:pb-20 lg:place-items-center '>
+                <div className='col-span-12 mt-20 lg:col-span-6 '>
+                    <h2 className='text-white font-roboto font-bold text-2xl md:text-4xl md:text-center md:leading-normal lg:text-left '>
                         Get our stories delivered from us to your inbox weekly.
                     </h2>
-                    <div className='w-full max-w-[494px] mt-12 space-y-3 mx-auto'>
+                    <div className='w-full max-w-[494px] mt-12 space-y-3 mx-auto md:space-y-0 md:flex md:items-center md:space-x-2 lg:mx-0 '>
                         <input type="text" className='px-4 py-3 rounded-lg w-full placeholder:text-dark-light' placeholder='Your Email'/>
-                        <button className='px-4 py-3 rounded-lg w-full bg-primary text-white font-bold'>Get Started</button>
+                        <button className='px-4 py-3 rounded-lg w-full bg-primary text-white font-bold md:w-fit md:whitespace-nowrap'>Get Started</button>
                     </div>
-                    <p className='text-dark-light text-sm leading-7 mt-6'>
-                        <span className='font-bold italic text-[#B3BAC5]'>Get a response tomorrow</span> if you submit by 9pm today. If we received after 9pm will get a response the following day
+                    <p className='text-dark-light text-sm leading-7 mt-6 md:text-center md:text-base lg:text-left '>
+                        <span className='font-bold italic text-[#B3BAC5] md:not-italic md:font-normal md:text-dark-light'>Get a response tomorrow</span> if you submit by 9pm today. If we received after 9pm will get a response the following day
                     </p>
                 </div>
-                <div className='col-span-12 hidden mt-[70px] md:block'>
-                    <div className={`items-center rounded-xl overflow-hidden shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] hover:shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)] `}>
-                        <img src={images.cta} alt="Articles"  className='w-full object-fill object-center h-auto md:h-52 lg:h-48 xl:h-60'/>
-                        <div className='p-5'>
-                            <h2 className='font-roboto font-bold text-xl text-dark-soft md:text-2xl lg:text-[28px]'>
-                                Future of work
-                            </h2>
-                            <p className='text-dark-light mt-3 text-sm md:text-lg'>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis?
-                            </p>
-                        </div>
+                <div className='col-span-12 hidden mt-[70px] md:block md:order-first lg:col-span-6 lg:order-last'>
+                    <div className='w-3/4 mx-auto relative'>
+                        <div className='w-1/2 h-1/2 bg-[#FC5A5A] rounded-lg absolute top-[10%] -right-[8%]'/>
+                        <div className='w-1/2 h-1/2 bg-white rounded-lg opacity-[.06] absolute -bottom-[10%] -left-[8%]'/>
+                        
+                        <div className="w-full rounded-xl bg-white p-3 z-[1] relative">
+                            <img src={images.cta} alt="Articles"  className='w-full object-cover object-center h-auto md:h-52 lg:h-48 xl:h-60'/>
+                            <div className='p-5'>
+                                <h2 className='font-roboto font-bold text-xl text-dark-soft md:text-2xl lg:text-[28px]'>
+                                    Future of work
+                                </h2>
+                                <p className='text-dark-light mt-3 text-sm md:text-lg'>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis?
+                                </p>
+                            </div>
                     </div>
+                    </div>
+                    
                 </div>
             </div>
         </section>
